071a8b2ccc659dad7d03d61e068e1d7d951e16e6
Valentino Lugli <valentinolugli@gmail.com>
2021-02-21 19:45:47 +0100

    [feat] YAML Support for input
    Changes:
    	- Added support for YAML formatted input for foods, so that many food can be entered at once.
    
    **Many things to do**

--- code/menuMaker.py
+++ code/menuMaker.py
@@ -15,6 +15,9 @@ import sys
 #   Get hash functions
 import hashlib
 
+#   YAML Parser
+import yaml
+
 
 # Functions
 def addPlates(conn):
@@ -77,6 +80,82 @@ def addPlates(conn):
 def genMenu():
     print("...here....")
 
+def loadPlates(conn):
+    print("---- LOADING PLATES FROM FILE ----")
+
+    cursor = conn.cursor()
+
+    path = input(">YAML file path: ")
+
+    with open(path,'r') as file:
+        food = yaml.full_load(file)
+
+    for item in food:
+
+        # Get the name from the key of the dictionary, by casting it to a list.
+        actFoodName = list(item)[0]
+
+        # Generate the ID by hashing the name.
+        actFoodID = hashlib.sha256(actFoodName.encode())
+
+
+        cursor.execute('SELECT idFood FROM food WHERE idFood = ?', (actFoodID.hexdigest(),))
+
+        if(not cursor.fetchone()):
+            # The rest of the items for this particular item are on a nested dictionary.
+            itemDetails = item.get(actFoodName)
+
+            actUniqueFood = itemDetails.get('isUnique')
+
+            schedule = itemDetails.get('schedule')
+            
+            # Get the values of the schedule as booleans.
+            breakBit = schedule[0].get('b')
+            lunchBit = schedule[1].get('l')
+            dinneBit = schedule[2].get('d')
+
+            # These values concatenated indicate the schedule as a binary value; convert from 'True' to string '1' or '0', 
+            # concatenate and convert to base 10,
+            actTimeOfDay = int(str(int(breakBit)) + str(int(lunchBit)) + str(int(dinneBit)), 2)
+
+            cursor.execute('INSERT INTO food VALUES (?, ?, ?, ?, 0)', (actFoodID.hexdigest(), actFoodName, actUniqueFood, actTimeOfDay))
+
+            print("Food [", actFoodName, "] added.")
+            # Add the ingredients to the database if not already there.
+            ingr = itemDetails.get('ingr')
+            for i in ingr:
+                igrName = list(i.keys())[0]
+                igrID = hashlib.sha256(igrName.encode())
+
+                cursor.execute('SELECT idIngr FROM ingredient WHERE idIngr = ?', (igrID.hexdigest(),))
+
+                if(not cursor.fetchone()):
+                    cursor.execute('INSERT INTO ingredient VALUES (?, ?, 0)', (igrID.hexdigest(), igrName))
+                    print("\tIngredient [", igrName, "] added.")
+                else:
+                    print("\tIngredient [", igrName,"] already exists on database. Skipped.")
+
+                auxList = (str(list(i.values())[0])).split()
+
+                igrQuantity = auxList[0]
+
+                if(len(auxList) > 1):
+                    igrUnit = auxList[1]
+                else:
+                    igrUnit = ''
+
+                cursor.execute('INSERT INTO madeup VALUES (?, ?, ?, ?)', (actFoodID.hexdigest(), igrID.hexdigest(), igrQuantity, igrUnit))
+
+        else:
+            print("Food [", actFoodName, "] already exists on database. Skipped.")
+
+    conn.commit()
+
+
+
+
+
+
 #   MAIN FUNCTION
 def main():
 
@@ -95,13 +174,16 @@ def main():
         while(running):
             print("---MENU MAKER---")
             print("1- Add plate\n"+
-            "2- Generate menu\n"+
+            "2- Load plates from file\n"+
+            "3- Generate a menu\n"
             "0- Exit")
             case = int(input("> "))
 
             if case == 1:
                 addPlates(conn)
             elif case == 2:
+                loadPlates(conn)
+            elif case == 3:
                 genMenu()
             elif case == 0:
                 running = False
